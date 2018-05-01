70da7f2e4e393e847c0f33e2adbc4504a029bbb0
nathan5x <walkwithsabari@gmail.com>
2018-05-01 16:04:22 -0700

    Menus updated

--- Sketch-SelectPlus.sketchplugin/Contents/Sketch/external/refs.js
+++ Sketch-SelectPlus.sketchplugin/Contents/Sketch/external/refs.js
@@ -4,7 +4,7 @@ const References = {
     STAR_GITHUB: "https://github.com/nathan5x/Sketch-SelectPlus/stargazers",
     VERSION: "https://github.com/nathan5x/Sketch-SelectPlus/blob/master/versions.md",
     BUG_URL: "https://github.com/nathan5x/Sketch-SelectPlus/issues/new?title=Bug",
-    GUIDE_URL: "https://github.com/nathan5x/Sketch-SelectPlus/blob/master/README.md",
+    GUIDE_URL: "https://github.com/nathan5x/Sketch-SelectPlus/blob/master/Guides/README.md",
     FEEDBACK_URL: "https://github.com/nathan5x/Sketch-SelectPlus/issues/new?title=Cool"
 }
 
